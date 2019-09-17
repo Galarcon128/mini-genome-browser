1fb047484e734cb94704cc0a7df48b98629363b8
Gabriel CCG <galarcon@ccg.unam.mx>
2019-09-17 15:07:50 -0500

    Primera rev estable

--- src/index.js
+++ src/index.js
@@ -1,38 +1,99 @@
+import * as d3 from 'd3'
 import React, { Component } from 'react'
-import GeneData from './inner_Components/resources/GeneData.json'
-import DrawGene from './inner_Components/GenomeBrowserMINI'
+import PropTypes from 'prop-types'
 
-export default class MiniGB extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      query: '',
-      gen: {},
-      genes: []
+const W = 500
+const H = 500
+
+export default class DrawGeneInfo extends Component {
+  static propTypes = {
+    GeneData: PropTypes.object
+  }
+
+  componentDidMount() {
+    this.draw(this.props.GeneData)
+  }
+
+  draw (GeneData) {
+    d3.select('#visual').select('svg').remove()
+    const svg = d3.select('#visual').append('svg')
+      .attr('width', W + 20)
+      .attr('height', H + 20)
+    // eslint-disable-next-line no-unused-vars
+    let img = svg.append('svg:image')
+      .attr('x', 0)
+      .attr('y', 0)
+      .attr('width', W)
+      .attr('height', H)
+      .attr('fill', 'cyan')
+      .attr('xlink:href', 'https://i.ibb.co/xHMV2N3/genome-IMG.png')
+    // eslint-disable-next-line no-unused-vars
+    if (JSON.stringify(GeneData) !== JSON.stringify({})) {
+      // eslint-disable-next-line no-unused-vars
+      let text = svg.append('text').text(GeneData.name).attr('text-align', 'left')
+        .attr('x', (W / 2) / 2)
+        .attr('y', W / 2)
+        .attr('font-family', 'sans-serif')
+        .attr('font-size', '50px')
+      text = svg.append('text').text(GeneData.id)
+        .attr('text-align', 'left')
+        .attr('x', (W / 2) / 2)
+        .attr('y', (W / 2) - 50)
+        .attr('font-family', 'sans-serif')
+        .attr('fill', 'blue')
+        .attr('font-size', '35px')
+      text = svg.append('text').text('position left: ' + GeneData.posL)
+        .attr('text-align', 'left')
+        .attr('x', (W / 2) / 2)
+        .attr('y', (W / 2) + 50)
+        .attr('font-family', 'sans-serif')
+        .attr('font-size', '30px')
+      text = svg.append('text').text('position Rigth: ' + GeneData.posR)
+        .attr('text-align', 'left')
+        .attr('x', (W / 2) / 2)
+        .attr('y', (W / 2) + 100)
+        .attr('font-family', 'sans-serif')
+        .attr('font-size', '30px')
+      text = svg.append('text').text('Strand: ' + GeneData.strand)
+        .attr('text-align', 'left')
+        .attr('x', (W / 2) / 2)
+        .attr('y', (W / 2) + 150)
+        .attr('font-family', 'sans-serif')
+        .attr('font-size', '30px')
+      animateGenome()
+    } else {
+      // eslint-disable-next-line no-unused-vars
+      let text = svg.append('text').text('Item not found')
+        .attr('text-align', 'left')
+        .attr('x', (W / 2) / 2)
+        .attr('y', (W / 2) - 50)
+        .attr('font-family', 'sans-serif')
+        .attr('font-size', '30px')
     }
-  }
-  _onChangeQuery = (event) => {
-    this.setState({ query: event.target.value })
-    this.setState({ gen: this._onS(event.target.value) })
-  }
-
-  _onS = (s) => {
-    let find = GeneData.Genes.find(Genes => Genes.name === s)
-    console.log(find)
-    return find
+    function animateGenome() {
+      let duration = 2500
+      svg.select('#genome')
+        .transition()
+        .duration(duration)
+        .attr('transform-origin', (W / 2) + ' ' + (W / 2))
+        .attr('transform', 'rotate(20)')
+        .transition()
+        .duration(duration)
+        .attr('transform', 'rotate(0)')
+        .on('end', animateGenome)
+        // at end, call it again to create infinite loop
+    }
   }
 
   render() {
     return (
-      <div>
-        <h1>Gene Search </h1>
-        <input type='text' name='TitleMap'
-          value={this.state.query}
-          onChange={(event) => this._onChangeQuery(event)} />
-        <button onClick={this._onS}>Search</button>
-        <button>config</button>
-        <DrawGene GeneData={this.state.gen} />
+      <div id='visual'>
+        {this.draw(this.props.GeneData)}
       </div>
     )
   }
+}
+
+DrawGeneInfo.defaultProps = {
+  GeneData: {}
 }
